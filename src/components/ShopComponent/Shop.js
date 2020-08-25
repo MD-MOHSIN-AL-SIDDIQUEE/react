74550639dd27254a31a45d9eb64f937e8fdaebca
mohsince13 <67540179+mohsince13@users.noreply.github.com>
2020-08-25 16:09:43 +0600

    first commit

---
+++ src/components/ShopComponent/Shop.js
@@ -0,0 +1,19 @@
+
+import React,{useState} from 'react';
+import fakeData from '../../fakeData';
+
+
+
+const Shop = () => {
+    const [data, setData] = useState(fakeData);
+
+  
+    return (
+   
+        <div>
+            <h1>This</h1>
+        </div>
+    );
+};
+
+export default Shop;
